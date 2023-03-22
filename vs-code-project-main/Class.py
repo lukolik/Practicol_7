ca49cc93daa07e08ea999403187eb78e5347b3a5
lukolik <110198144+lukolik@users.noreply.github.com>
2023-03-22 20:33:55 +0300

    Add files via upload

---
+++ vs-code-project-main/Class.py
@@ -0,0 +1,142 @@
+# class Human():
+
+#     def __init__(self, fio, birsday, phone, city, country, address):
+#         self.fio = fio
+#         self.birsday = birsday
+#         self.phone = phone
+#         self.city = city
+#         self.country = country
+#         self.address = address
+    
+#     def setFio(self):
+#         self.fio = input("enter FIO - ")
+    
+#     def setBirsday(self):
+#         self.birsday = input("enter birsday - ")
+    
+
+#     def setPhone(self):
+#         self.phone = input("enter phone - ")
+    
+
+#     def setCity(self):
+#         self.city = input("enter city - ")
+    
+
+#     def setCountry(self):
+#         self.country = input("enter country - ")
+
+#     def setAddres(self):
+#         self.address = input("enter address - ")
+    
+#     def setAll(self):
+#         self.fio = input("enter FIO - ")
+#         self.birsday = input("enter birsday - ")
+#         self.phone = input("enter phone - ")
+#         self.city = input("enter city - ")
+#         self.country = input("enter country - ")
+#         self.address = input("enter address - ")
+
+#     def getFio(self):
+#         print(self.fio)
+
+#     def getBirsday(self):
+#         print(self.birsday)
+    
+#     def getPhone(self):
+#         print(self.phone)
+    
+#     def getCity(self):
+#         print(self.city)
+    
+#     def getCountry(self):
+#         print(self.country)
+
+#     def getAddres(self):
+#         print(self.address)
+
+#     def out(self):
+#          print("Fio = ", self.fio)
+#          print("Brisday = ", self.birsday)
+#          print("Phone = ",self.phone)
+#          print("City = ", self.city)
+#          print("Country = ", self.country)
+#          print("Address = ", self.address)
+
+# msiha = Human("misha", "16.10.2000", "+7988865423", "Sochi", "RF", "Mishaon")
+
+# msiha.out()
+# msiha.setFio()
+# msiha.out()
+
+
+# class City():
+
+#     def __init__(self, name, rigion, country, index_city, city_phone):
+#         self.name = name
+#         self.rigion = rigion
+#         self.country = country
+#         self.index_city = index_city
+#         self.city_phone = city_phone
+       
+    
+#     def setName(self):
+#         self.name = input("enter FIO - ")
+    
+#     def setRigion(self):
+#         self.rigion = input("enter birsday - ")
+    
+
+#     def setCountry(self):
+#         self.country = input("enter phone - ")
+    
+
+#     def setindex_city(self):
+#         self.index_city = input("enter city - ")
+    
+
+#     def setCity_phone(self):
+#         self.city_phone = input("enter country - ")
+
+   
+    
+#     def setAll(self):
+#         self.name = input("enter name - ")
+#         self.rigion = input("enter rigion - ")
+#         self.country = input("enter country - ")
+#         self.index_city = input("enter index - ")
+#         self.city_phone = input("enter phone_city - ")
+
+    
+#     def getName(self):
+#         print(self.name)
+
+#     def getRigion(self):
+#         print(self.rigion)
+    
+#     def getCountry(self):
+#         print(self.country)
+    
+#     def getIndex_City(self):
+#         print(self.index_city)
+    
+#     def getCity_Phone(self):
+#         print(self.city_phone)
+
+    
+
+#     def out(self):
+#          print("name = ", self.name)
+#          print("rigion = ", self.rigion)
+#          print("country = ",self.country)
+#          print("index_city = ", self.index_city)
+#          print("city_phone = ", self.city_phone)
+
+
+# class Country:
+#     def __init__(self, name, rigion, country, index_city, city_phone):
+#         self.name = name
+#         self.rigion = rigion
+#         self.country = country
+#         self.index_city = index_city
+#         self.city_phone = city_phone
